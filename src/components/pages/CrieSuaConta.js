81865aaa33b3f59e6ffb79c2cdbba08bd078b02e
Mario <duarte3108@gmail.com>
2022-07-19 12:17:58 -0300

    first commit

---
+++ src/components/pages/CrieSuaConta.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CrieSuaConta(){
+
+    return(
+        <div className="container mt-3">
+            <h1>Crie sua conta!</h1>
+        </div>
+    )
+}
